91019d88307645ca6fa6d8000fe137a07f69b841
Jeyson Aires <161245148+A1rez@users.noreply.github.com>
2024-06-21 16:21:06 -0300

    Update code.py

--- code.py
+++ code.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from tkinter import Tk, filedialog
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
