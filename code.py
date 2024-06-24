ffefaf527cab2efebe76eb6fafd4b5be692e023d
Jeyson Aires <161245148+A1rez@users.noreply.github.com>
2024-06-24 20:13:03 -0300

    Update code.py

--- code.py
+++ code.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from tkinter import Tk, filedialog
 
 # Importing machine learning tools from scikit-learn
 from sklearn.model_selection import train_test_split
